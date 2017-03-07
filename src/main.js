414f1dbc0fc51152f063367c67c02018424a5322
JeroenVinke <jeroenvinke17@gmail.com>
2017-03-07 21:25:01 +0100

    initial

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import environment from './environment';
+import 'my-dependency';
+import 'my-dependency/resource1';
+
+
+//Configure Bluebird Promises.
+Promise.config({
+  warnings: {
+    wForgottenReturn: false
+  }
+});
+
+export function configure(aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .feature('resources');
+
+  if (environment.debug) {
+    aurelia.use.developmentLogging();
+  }
+
+  if (environment.testing) {
+    aurelia.use.plugin('aurelia-testing');
+  }
+
+  aurelia.start().then(() => aurelia.setRoot());
+}
